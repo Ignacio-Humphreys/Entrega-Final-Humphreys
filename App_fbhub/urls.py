0bf81e949f4245a0b4a9540a12d7b56733c8da02
Ignacio-Humphreys <attshumphreys@gmail.com>
2023-02-16 00:08:09 -0300

    Creados views, punto seguro

--- App_fbhub/urls.py
+++ App_fbhub/urls.py
@@ -3,5 +3,10 @@ from App_fbhub import views
 
 urlpatterns = [
     
-    
+    path("", views.inicio, name="Inicio"),
+    path("Equipos/", views.equipos, name="Equipos"),
+    path("Estadios/", views.estadios, name="Estadios"),
+    path("Jugadores/", views.jugadores, name="Jugadores"),
+    path("Agregar/", views.agregarItem, name="Agregar"),
+    path("Dummy/", views.dummyPage, name="Dummy"),    
 ]
--- App_fbhub/views.py
+++ App_fbhub/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from App_fbhub.models import *
+from App_fbhub.forms import *
 
 # Create your views here.
+def inicio(request):
+    return render(request, "../templates/inicio.html")
+
+def equipos(request):
+    return render(request, "../templates/equipos.html")
+
+def estadios(request):
+    return render(request, "../templates/estadios.html")
+
+def jugadores(request):
+    return render(request, "../templates/jugadores.html")
+
+def dummyPage(request):
+    return render(request, "../templates/dummy.html")
+
+def agregarItem(request):
+    return render(request, "../templates/agregar.html")
