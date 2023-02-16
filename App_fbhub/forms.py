ff1cdafe31b14d20f1fb1e8a61698062db4c43a7
Ignacio-Humphreys <attshumphreys@gmail.com>
2023-02-16 00:49:55 -0300

    Modificación del admin + creación de forms + modificación de clase Estadio

---
+++ App_fbhub/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+class FormularioEquipos(forms.Form):
+    nombre = forms.CharField(max_length=40)
+    cant_jugadores = forms.IntegerField()
+    fundacion = forms.IntegerField()
+    estadio = forms.CharField(max_length=40)
+    colores = forms.CharField(max_length=40)
+
+class FormularioJugador(forms.Form):
+    nombre = forms.CharField(max_length=40)
+    apellido = forms.CharField(max_length=40)
+    pie_fav = forms.CharField(max_length=1)
+    posicion = forms.CharField(max_length=20)
+
+class FormularioEstadio(forms.Form):
+    capacidad = forms.IntegerField()
+    ubicacion = forms.CharField(max_length=40)
--- App_fbhub/models.py
+++ App_fbhub/models.py
@@ -7,13 +7,20 @@ class Equipo(models.Model):
     fundacion = models.IntegerField()
     estadio = models.CharField(max_length=40)
     colores = models.CharField(max_length=40, default="")
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Jugadores: {self.cant_jugadores} - Fundación: {self.fundacion} - Estadio: {self.estadio} - Colores: {self.colores}"
 
 class Jugador(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
     pie_fav = models.CharField(max_length=1)
     posicion = models.CharField(max_length=20)
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Pie favorito: {self.pie_fav} - Posición: {self.posicion}"
 
 class Estadio(models.Model):
+    nombre = models.CharField(max_length=40, default="")
     capacidad = models.IntegerField()
     ubicacion = models.CharField(max_length=40)
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Capacidad: {self.capacidad} - Ubicación: {self.ubicacion}"
